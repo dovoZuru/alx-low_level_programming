bdeef353ab6d222a35ef25fe040b3392b15028ca
dovoZuru <dovo.fav@aol.com>
2022-09-09 16:50:32 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 /**
  * main - print if the number is postive, zero, or negative
@@ -11,12 +12,13 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	putchar(ch);
+        putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-	putchar(ch);
+        putchar(ch);
 }
 putchar('\n');
 return (0);
+}
 }
