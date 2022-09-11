63fe6d4baa6a955ce0090f6bdbea58f9bff042ca
dovoZuru <dovo.fav@aol.com>
2022-09-11 14:04:24 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ * Return: 0.
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-if (ch != 'e' && ch != 'q')
-{
-	putchar(ch);
-}
-}
-putchar('\n');
-return (0);
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		puchar(alpha);
+
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		puchar(alpha);
+
+	puchar('\n');
+
+	return (0);
 }
