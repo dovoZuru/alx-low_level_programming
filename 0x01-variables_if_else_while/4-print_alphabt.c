16c41d04d734654be88542cf69ea085b23e658b9
dovoZuru <dovo.fav@aol.com>
2022-09-11 14:19:24 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,20 @@
-
 #include <stdio.h>
+
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * main - This code prints the alphabet in lowercase, except for q and e.
+ * Return: 0.
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-        putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-        putchar(ch);
-}
-putchar('\n');
-return (0);
-}
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
