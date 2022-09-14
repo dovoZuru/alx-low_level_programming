9e75d2d2c58308a21198e7e6614d112d6f146887
dovoZuru <dovo.fav@aol.com>
2022-09-13 17:02:55 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
+ * Return: 0.
+ */
+void print_alphabet_x10(void)
+{
+	int c = 0;
+	char alpha;
+
+	while (c++ <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+	}
+}
